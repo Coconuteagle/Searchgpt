0502bac3dd60e5fbd46fe5ed75d16ffda84f26d9
Coconuteagle <131874183+Coconuteagle@users.noreply.github.com>
2025-04-04 12:58:53 +0900

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,17 @@ window.apiKeys = {
       googleApiKey: "AIzaSyBXoERTUBALRJl6k2OsJ5tGnElcSZMsBfA",
       searchEngineId: "552ad4c6d70ca433e"
     },
+    {
+      googleApiKey: "AIzaSyDL01M_fwCJGve-We30Lx271mHpruJofpM",
+      searchEngineId: "c740225da5cb9464d"
+    },
+    {
+      googleApiKey: "",
+      searchEngineId: ""
+      {
+      googleApiKey: "",
+      searchEngineId: ""
+    },
     // ...
   ],
   GEMINI_API_KEY: "AIzaSyCptpJ68R5lyJPduY8rtqUXR9Ij7F4puoE"
