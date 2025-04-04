d47ea7d14b901e6eac20f01665284f2691015305
Coconuteagle <131874183+Coconuteagle@users.noreply.github.com>
2025-04-04 10:57:23 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 // config.js
 window.apiKeys = {
-  // 원하는 만큼 추가
   GOOGLE_SEARCH_CONFIGS: [
     {
       googleApiKey: "AIzaSyDndNAfYE6-_tpcQk5mRlbNGduvFN_WF2o",
@@ -10,10 +9,7 @@ window.apiKeys = {
       googleApiKey: "AIzaSyBXoERTUBALRJl6k2OsJ5tGnElcSZMsBfA",
       searchEngineId: "552ad4c6d70ca433e"
     },
-    // 필요하다면 더 추가
+    // ...
   ],
-
-  // Google Gemini API Key (For generation)
-  GEMINI_API_KEY: 'AIzaSyCptpJ68R5lyJPduY8rtqUXR9Ij7F4puoE' 
+  GEMINI_API_KEY: "AIzaSyCptpJ68R5lyJPduY8rtqUXR9Ij7F4puoE"
 };
-
